9abcad3bb64cc473ec4ab99562f213e8aa0a37ce
jgch <43004204+juagacha@users.noreply.github.com>
2025-06-03 10:42:52 -0500

    new panels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    public int sanityOfPlayer; //cordura del jugador
+    private int maxValueOfSanity=100; // maxima cordura
+    public int numberActualScene; //guarda el numero de la escena actual
+
+    //ultima escena
+    public Vector3 lastPosition; // posicion del jugador en la ultima escena
+
+    //List<GameObject> lista para el inventario
+
+    public bool isGameActive;
+    [SerializeField] GameObject gameOverPannel;
+
     private void Awake()
     {
-
         if (Instance == null)
         {
             Instance = this;
@@ -28,18 +41,55 @@ public class GameManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void ChangeLanguage(int option)
     {
         if (option == 0 || option == 1)
         {
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[option];
         }
-        
     }
+
+    public void IncreaseSanity(int value) 
+    {
+        if (sanityOfPlayer < maxValueOfSanity)
+        { 
+            sanityOfPlayer += value;
+            if (sanityOfPlayer>=maxValueOfSanity) 
+            {
+                sanityOfPlayer=maxValueOfSanity;
+            }
+        }
+    }
+
+    public void DecreaseSanity(int value)
+    {
+        if (sanityOfPlayer > 0)
+        {
+            sanityOfPlayer -= value;
+            if (sanityOfPlayer <=0 )
+            {
+                sanityOfPlayer = 0;
+            }
+        }
+    }
+
+    public void setLastPosition(Vector3 actualPosition) 
+    {
+        lastPosition = actualPosition; // ultima posicion del player
+    }
+
+    public void SetActualScene(int value) 
+    {
+        numberActualScene=value; // ultima escena en la que estuvo
+    }
+
+    public void GameOver()
+    {
+        gameOverPannel.SetActive(true);
+        //SceneManager.LoadScene(1);// carga UI scene
+        isGameActive = false;
+
+    }
+
+
 }
---
+++ Assets/Scripts/PausePanel.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class PausePanel : MonoBehaviour
+{
+    [SerializeField] GameObject pausePannel; //panel activo
+
+    public void QuitButton() 
+    {
+        pausePannel.SetActive(false);
+        //SceneManager.LoadScene(1);// carga primera escena titulo
+    }
+
+    public void ResumeButton() 
+    {
+        if (GameManager.Instance != null) 
+        {
+            Time.timeScale = 1.0f;
+            pausePannel.SetActive(false);
+            int temporalscene = GameManager.Instance.numberActualScene; // llama la ultima escena en la que estuvo
+            Vector3 lastposition = GameManager.Instance.lastPosition;// llama la posicion del jugador en la ultima escena
+            //player.transform.position = lastposition; //asigna la posicion al player 
+            SceneManager.LoadScene(temporalscene);// carga ultima escena en la que estuvo
+        }
+        
+    }
+
+}
